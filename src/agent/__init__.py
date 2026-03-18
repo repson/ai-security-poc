e736a3f5bebeb72a66f3373adbab7c4aab15f97e
Isaac Peña <info@ipenat.com>
2026-03-18 21:29:29 +0100

    Delete guardrails and agent folders

--- src/agent/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Agentic assistant package."""
-
-from .agent import Agent
-from .tools import TOOL_REGISTRY, dispatch_tool
-
-__all__ = ["Agent", "TOOL_REGISTRY", "dispatch_tool"]
--- src/guardrails/__init__.py
+++
@@ -1,5 +0,0 @@
-"""NeMo Guardrails integration package."""
-
-from .guardrails_agent import GuardedAgent
-
-__all__ = ["GuardedAgent"]
